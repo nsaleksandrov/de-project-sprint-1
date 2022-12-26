2e58c7dc887b5ebe9938cabdb26fff9813ebde8b
Nikita Aleksandrov <nikitkaalexandrov@yandex.ru>
2022-12-26 03:08:24 +0300

    Вторая итерация

--- project/task2/realization.sql
+++ project/task2/realization.sql
@@ -1,12 +1,12 @@
--- добавьте код сюда
-CREATE OR REPLACE VIEW analysis.Orders AS 
+create or replace view  analysis.orders_view as
 select o.order_id ,
        o.order_ts,
        o.user_id ,
        o.bonus_payment,
        o.cost ,
        o.bonus_grant ,
-       o3.key as sataus
+       o.status 
+       o3.key as status_key
 from production.orders o 
 join (select *, 
      row_number() over (partition by order_id order by dttm desc) rn
@@ -14,4 +14,3 @@ from production.orderstatuslog o2) o2
 on o.order_id = o2.order_id and o.status = o2.status_id 
 left join production.orderstatuses o3 on o2.status_id = o3.id 
 where rn =1 
-order by o.order_id 
