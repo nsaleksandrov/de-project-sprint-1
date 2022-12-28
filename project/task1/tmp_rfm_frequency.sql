031a41c76b035c57ceaf8b542314a60feaf8f39d
Nikita Aleksandrov <nikitkaalexandrov@yandex.ru>
2022-12-28 10:47:46 +0300

    Пятая итерация

--- project/task1/tmp_rfm_frequency.sql
+++ project/task1/tmp_rfm_frequency.sql
@@ -1,20 +1,32 @@
-WITH AS tmp_rfm_frequency (
-select user_id,
-      NTILE(5) over (order BY COUNT(order_id)) as frequency
-     from  analysis."order" o
-WHERE o.status = 4 --closed
-AND   o.order_ts >= '2022-01-01'    
-group by user_id)
+WITH tmp_rfm_frequency AS ( 
 
-INSERT INTO analysis.tmp_rfm_frequency
-SELECT u.id as user_id,
-       frequency
-FROM analysis."users" u 
-LEFT JOIN tmp_rfm_frequency trf
-ON u.id = trf.user_id
-WHERE u.id IN (SELECT DISTINCT user_id
-                    FROM analysis."order" o
-                    WHERE o.order_ts >='2022-01-01')
+select user_id 
 
+     from  production.orders o 
 
+WHERE o.status = 4 --closed 
 
+AND   o.order_ts >= '2022-01-01'     
+
+group by user_id
+) 
+ 
+
+INSERT INTO analysis.tmp_rfm_frequency 
+
+SELECT u.id as user_id, 
+        NTILE(5) over (order BY COUNT(order_id)) as frequency
+
+FROM analysis."users" u  
+
+LEFT JOIN tmp_rfm_frequency trf 
+
+ON u.id = trf.user_id 
+
+left join analysis."orders" o
+
+on u.id = o.user_id
+
+where o.order_ts >='2022-01-01'
+
+group by u.id
